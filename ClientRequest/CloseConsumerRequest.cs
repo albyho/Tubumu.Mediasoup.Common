8dfccb63fa8af187c48768eec4276384c8799546
alby <alby@foxmail.com>
2021-09-09 14:02:37 +0800

    Nothing.

--- ClientRequest/CloseConsumerRequest.cs
+++ ClientRequest/CloseConsumerRequest.cs
@@ -1,9 +1,13 @@
-﻿namespace Tubumu.Mediasoup
+﻿using System.Collections.Generic;
+
+namespace Tubumu.Mediasoup
 {
     public class CloseConsumerRequest
     {
-        public string PeerId { get; set; }
+        public string? PeerId { get; set; }
 
-        public string ProducerIds { get; set; }
+        public HashSet<string>? ProducerIds { get; set; }
+
+        public HashSet<string>? ConsumerIds { get; set; }
     }
 }
